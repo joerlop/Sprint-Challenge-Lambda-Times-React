6ba9fdd88b9f72fec9faf1aac9c8164954429c0e
joerlop <jj.erlich155@gmail.com>
2019-05-03 11:36:29 -0500

    Header converted to styled components

---
+++ lambdatimes/src/Styles/Header/HeaderContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: row;
+    background-color: #fff;
+    border-bottom: 1px solid lightgrey;
+    width: 100%;
+    height: 120px;
+    margin-top: 44px;
+    padding-bottom: 15px;
+
+    @media (min-width: 1280px) {
+          width: 1280px;
+      }
+`;
+
+export default HeaderContainer;
--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
+import HeaderContainer from "../Styles/Header/HeaderContainer"
+import HeaderH1 from "../Styles/Header/HeaderH1"
+import HeaderInfo from "../Styles/Header/HeaderInfo"
+
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+    <HeaderContainer>
+      <HeaderInfo temp={false}>SMARCH 32, 2018</HeaderInfo>
+      <HeaderH1>Lambda Times</HeaderH1>
+      <HeaderInfo temp>98°</HeaderInfo>
+    </HeaderContainer>
   )
 }
 

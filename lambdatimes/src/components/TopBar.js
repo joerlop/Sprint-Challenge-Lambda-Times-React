4df311c94658bd147b15f936e6b479d0c3df5c34
joerlop <jj.erlich155@gmail.com>
2019-05-03 12:47:42 -0500

    Tried doing the HOC stretch but couldn't get reactstrap styles to work

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -9,25 +9,24 @@ import ContainerCenterSpan from "../Styles/TopBar/ContainerCenterSpan"
 import ContainerRight from "../Styles/TopBar/ContainerRight"
 import ContainerRightSpan from "../Styles/TopBar/ContainerRightSpan"
 
-
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
-const TopBar = () => {
-  return (
-    <TopBarContainer>
-      <Container>
-        <ContainerLeft>
-          <ContainerLeftSpan>TOPICS</ContainerLeftSpan><ContainerLeftSpan>SEARCH</ContainerLeftSpan>
-        </ContainerLeft>
-        <ContainerCenter>
-          <ContainerCenterSpan>GENERAL</ContainerCenterSpan><ContainerCenterSpan>BROWNBAG</ContainerCenterSpan><ContainerCenterSpan>RANDOM</ContainerCenterSpan><ContainerCenterSpan>MUSIC</ContainerCenterSpan><ContainerCenterSpan>ANNOUNCEMENTS</ContainerCenterSpan>
-        </ContainerCenter>
-        <ContainerRight>
-          <ContainerRightSpan>LOG IN</ContainerRightSpan>
-        </ContainerRight>
-      </Container>
-    </TopBarContainer>
+const TopBar = props => {
+    return (
+      <TopBarContainer>
+        <Container>
+          <ContainerLeft>
+            <ContainerLeftSpan>TOPICS</ContainerLeftSpan><ContainerLeftSpan>SEARCH</ContainerLeftSpan>
+          </ContainerLeft>
+          <ContainerCenter>
+            <ContainerCenterSpan>GENERAL</ContainerCenterSpan><ContainerCenterSpan>BROWNBAG</ContainerCenterSpan><ContainerCenterSpan>RANDOM</ContainerCenterSpan><ContainerCenterSpan>MUSIC</ContainerCenterSpan><ContainerCenterSpan>ANNOUNCEMENTS</ContainerCenterSpan>
+          </ContainerCenter>
+          <ContainerRight>
+            <ContainerRightSpan onClick={() => props.toggle()}>LOG IN</ContainerRightSpan>
+          </ContainerRight>
+        </Container>
+      </TopBarContainer>
   )
 }
 
